715d540e62deaae1ce05cc96239c60e0e5d5f596
gghatano <adad.0405@gmail.com>
2020-02-11 00:49:16 +0000

    long MAXはちゃんと大きな値にする 10^18までいける

--- dp_c.cpp
+++ dp_c.cpp
@@ -6,7 +6,7 @@ int main(){
   long N,K;
   cin >> N >> K;
 
-  long MAX = 100000000000;
+  long MAX = 100000000;
   vector<long> h(N);
   for(int i = 0; i < N; i++){
     cin >> h.at(i);
@@ -14,16 +14,12 @@ int main(){
 
   vector<long> dp(N, MAX);
 
-  dp[0] = 0;
+  dp[0] = h.at(0);
 
   for(int i = 0; i < N; i++){
-    for(int j = 1; (j <= K) && (i+j < N) ; j++){
-      dp[i+j] = min(dp[i] + abs(h.at(i) - h.at(i+j)), dp[i+j]);
+    for(int j = 1; (j <= K) && (i + j < N); j++){
+      dp[i+j] = min(dp[i] + abs(h.at(i) - h.at(j)), dp[i+j]);
     }
-  }
-
-  for(int i = 0; i < N; i++){
-    cerr << dp[i] << endl;
   }
 
   cout << dp[N-1] << endl;
