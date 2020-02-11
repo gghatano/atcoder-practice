c2cb2b535ccb63f7944e6da1b5e9424e67f86c74
gghatano <adad.0405@gmail.com>
2020-02-11 01:22:37 +0000

    edu dp_c できた

--- dp_c.cpp
+++ dp_c.cpp
@@ -3,24 +3,35 @@
 using namespace std;
 
 int main(){
-  long N,K;
-  cin >> N >> K;
+  long N;
+  cin >> N;
 
-  long MAX = 100000000;
-  vector<long> h(N);
+  // 幸福度
+  vector<vector<long> > hp(N, vector<long>(3,0));
+  // 幸福度の総和の最大値
+  vector<vector<long> > dp(N, vector<long>(3,0));
+
   for(int i = 0; i < N; i++){
-    cin >> h.at(i);
+    cin >> hp.at(i).at(0) >> hp.at(i).at(1) >> hp.at(i).at(2);
+  }
+  
+  // 1日目
+  for(int j = 0; j < 3; j++){
+    dp.at(0).at(j) = hp.at(0).at(j);
   }
 
-  vector<long> dp(N, MAX);
-
-  dp[0] = h.at(0);
-
-  for(int i = 0; i < N; i++){
-    for(int j = 1; (j <= K) && (i + j < N); j++){
-      dp[i+j] = min(dp[i] + abs(h.at(i) - h.at(j)), dp[i+j]);
+  for(int i = 0; i < N -1; i++){
+    for(int j = 0; j <= 2; j++){
+      for(int k = 0; k <=2; k++){
+        if(k!=j) dp[i+1][j] = max(dp[i+1][j], dp[i][k] + hp[i+1][j]);
+      }
     }
   }
+  long ans = 0;
+  for(int j = 0; j < 3; j++){
+    ans = max(ans, dp[N-1][j]);
+  }
+  cout << ans << endl;
 
-  cout << dp[N-1] << endl;
+
 }
