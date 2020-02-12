7cab141e6e7faa0c38125faeb62cef8629e01945
gghatano <adad.0405@gmail.com>
2020-02-12 12:44:00 +0900

    commit test

--- make.bash
+++ make.bash
@@ -1,18 +1,23 @@
 target=$1
 
-
+echo ""
 echo "=== compile ==="
-g++ -std=c++11 -o $target $target.cpp
-
+g++ -std=c++11 -o $target $target.cpp 2>/tmp/cmperr.txt
+echo ""
+echo ""
+echo "=== cmperr === "
+cat /tmp/cmperr.txt
+echo ""
+echo ""
 echo "=== execute ==="
-./${target} < ${target}.txt 1> stdout.txt 2>stderr.txt
-
+./${target} < ${target}.txt 1> /tmp/stdout.txt 2>/tmp/stderr.txt
+echo ""
 echo ""
 echo "=== stderr === "
-cat stderr.txt
+cat /tmp/stderr.txt
 echo ""
 echo ""
 echo "=== stdout === "
-cat stdout.txt
+cat /tmp/stdout.txt
 
 rm ${target}
